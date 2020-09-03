6161bf01f1f487382a81ea91980a37d7211cf362
wajika <wajika@hotmail.com>
2020-09-03 10:42:17 +0800

    Update rum.js

--- src/rum.js
+++ src/rum.js
@@ -6,9 +6,9 @@ var apm = initApm({
   // Used on the APM Server to find the right sourcemap
   serviceVersion: '0.90',
   // Set custom APM Server URL (default: http://localhost:8200)
-  serverUrl: 'https://5fea15b641064bfa800ef7591069c865.apm.us-central1.gcp.cloud.es.io:443',
+  serverUrl: 'https://apmserver.lonsid.cn',
   // For distributed tracing to different origin (CORS)
-  // distributedTracingOrigins: ['http://localhost:8080'],
+  distributedTracingOrigins: ['https://apmserver.lonsid.cn']
 })
 
 export default apm;
